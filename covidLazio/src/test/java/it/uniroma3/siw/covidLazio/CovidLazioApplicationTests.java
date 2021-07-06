867ab5f07460a62352ed1750cd253c69c9cc8b63
danielepica <73017443+danielepica@users.noreply.github.com>
2021-07-06 12:31:39 +0200

    Pubblicazione progetto

---
+++ covidLazio/src/test/java/it/uniroma3/siw/covidLazio/CovidLazioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.covidLazio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidLazioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
